c19b4591971772b6d8f7f7b12c933b1030f131a5
fabian3-png <fabianramirezrosaz@gmail.com>
2026-04-21 07:26:08 -0600

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,31 @@
+ using System;
+class Animal
+{
+    public string ruido;
+    
+    static void respirar()
+    {
+        Console.WriteLine("los animales respiran");
+        
+    }
+    public void accion()
+    {
+        Console.WriteLine(" y " + ruido);
+    }
+    static void Main()
+    {
+        Animal pez= new Animal();
+        Animal perro= new Animal();
+        Animal gato= new Animal();
+        
+        pez.ruido= "los peces nadan";
+        perro.ruido="los perros ladran";
+        gato.ruido="los gatos maullan";
+        
+        Animal.respirar();
+        pez.accion();
+        perro.accion();
+        gato.accion();
+        
+    }
+}
